ebc6f0938967464f4ee9650a75842e4f2795bbec
Radek Matěj <radek@nikdo.cz>
2021-01-26 21:46:45 +0100

    Read port from env variable

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const { fetchData } = require('./fetchData')
 
+const port = process.env.PORT || '3030'
 const app = express()
+
 app.use(cors())
-const port = 3030
 
 app.get('/risk-score', (_req, res) => {
   fetchData()
@@ -13,4 +14,5 @@ app.get('/risk-score', (_req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
+  console.log(`Running with NODE_ENV set to ${process.env.NODE_ENV}`)
 })
