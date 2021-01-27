3c8eaf84507a54729e26865e192319027703fde4
Radek Matěj <radek@nikdo.cz>
2021-01-27 23:26:32 +0100

    Fetch data from UZIS

--- fetchData.js
+++ fetchData.js
@@ -1,16 +1,29 @@
+const { pipeline } = require('stream')
+const got = require('got')
+const stripBomStream = require('strip-bom-stream')
 const Papa = require('papaparse')
-const { createReadStream } = require('fs')
+
+const dataFileUrl = 'https://share.uzis.cz/s/BRfppYFpNTddAy4/download?path=%2F&files=pes_CR_verze2.csv'
 
 const fetchData = () => new Promise((resolve, reject) => {
-  const file = createReadStream('./data.csv', { encoding: 'utf8' })
-  Papa.parse(file, {
-    download: true,
-    header: true,
-    skipEmptyLines: true,
-    dynamicTyping: true,
-    complete: result => resolve(result.data),
-    error: reject
-  })
+  const data = []
+  pipeline(
+    got.stream(dataFileUrl),
+    stripBomStream(),
+    Papa.parse(Papa.NODE_STREAM_INPUT, {
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      encoding: 'utf8'
+    }),
+    err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    }
+  ).on('data', item => data.push(item))
 })
 
 module.exports = {
